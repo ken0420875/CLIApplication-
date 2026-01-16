82624ebeeb63b3ffc59cfd62aec25a2927d243e4
jesicamatul <evelinmtlj@gmail.com>
2026-01-16 12:17:36 -0500

    ledger

--- src/main/java/com/pluralsight/ShowLedger.java
+++ src/main/java/com/pluralsight/ShowLedger.java
@@ -5,38 +5,38 @@ import java.util.Scanner;
 public class ShowLedger {
 
     public static void showLedger() {
-        Scanner scanner = new Scanner(System.in);
-        boolean inLedgerPage = true;
-        while (inLedgerPage) {
-            System.out.println("\n-------Ledger Page ------- ");
-            System.out.print("A) All Entries ");
-            System.out.println("D)Deposits ");
-            System.out.println("P) Payments ");
-            System.out.println("R) Reports ");
-            System.out.println("H) Home page ");
-            System.out.print("Choose Your Choice: ");
 
+        String ledgerScreen = """
+                                    *************************************
+                                    *         Welcome to ledger screen   *
+                                    *************************************
+                                      Choose what you would like to display
+                
+                                    [A: All]             [D: Deposits ]
+                
+                                    [P: Payments]         [R:  Reports ]
+                                         [H: Back to home screen ]
+                """;
 
-            String choice = scanner.nextLine().toUpperCase();
-            // Not needed
-            switch (choice) {
-                case "A":
-                    System.out.println("Display all entries");
-                    break;
-                case "D":
-                    DisplayDeposits.displayDeposits();
-                    break;
-                case "P":
-                    DisplayPayments.displayPayments();
-                    break;
-                case "R":
-                    DisplayReports.displayReports();
-                    break;
-                case "H":
-                    inLedgerPage = false;
-                    break;
-            }
+           while(true){
+                System.out.println(ledgerScreen);
+                String choice = ConsoleHelper.promptForString("Enter your choice").toUpperCase();
 
+               switch (choice) {
+                   case "A" -> System.out.println("Display all entries");
+                   case "D" -> DisplayDeposits.displayDeposits();
+                   case "P" -> DisplayPayments.displayPayments();
+                   case "R" -> DisplayReports.displayReports();
+                   case "H" -> {
+                       System.out.println("Returning to home screen ...");
+                       break;
+                   }
+                   default -> System.out.println("Invalid choice. Please try again.");
+
+               }
+               if (choice.equals("H")) {
+                   break;
+               }
         }
 
     }
