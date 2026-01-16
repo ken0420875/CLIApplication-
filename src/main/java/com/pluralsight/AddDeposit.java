f57f833640249114fa81e475c49e9ce1e343ee1c
jesicamatul <evelinmtlj@gmail.com>
2026-01-16 11:27:58 -0500

    add depostit

--- src/main/java/com/pluralsight/AddDeposit.java
+++ src/main/java/com/pluralsight/AddDeposit.java
@@ -6,28 +6,43 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
+
 
 public class AddDeposit {
 
-    public static void addDeposit(Scanner scanner) {
+    public static void addDeposit() {
         LocalDate today = LocalDate.now();
         LocalTime time = LocalTime.now();
 
+        DateTimeFormatter dateFmt = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter timeFmt = DateTimeFormatter.ofPattern("HH:mm:ss");
 
-        String description =ConsoleHelper.promptForString("Please Enter the description:");
-        String vendorName =ConsoleHelper.promptForString("Please enter Vendor name: ");
-        double amountPaid = ConsoleHelper.promptForDouble("Please enter amount to be paid: ");
+//user input
+        String description =     ConsoleHelper.promptForString("Please Enter the description:");
+        String vendorName =  ConsoleHelper.promptForString("Please enter Vendor name: ");
+        double amount = ConsoleHelper.promptForDouble("Please enter amount to be paid: ");
         // used this multiple times
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("transactions.csv" , true))) {
-            writer.write(today.format(fmt)+ "|" + time + "|" + description + "|" + vendorName + "|" + amountPaid + "\n");
-            System.out.println("Deposit has went through");
+
+        try (BufferedWriter writer = new BufferedWriter(
+                new FileWriter("transactions.csv", true))) {
+
+            writer.write(
+                    today.format(dateFmt) + "|" +
+                            time.format(timeFmt) + "|" +
+                            description + "|" +
+                            vendorName + "|" +
+                            amount + "\n"
+            );
+
+            System.out.println("Deposit has gone through ");
+
         } catch (IOException ex) {
-            System.err.println("Error " + ex.getMessage());
+            System.err.println("Error: " + ex.getMessage());
         }
 
-//        System.out.println();
+
 
     }
 
 }
+ 
