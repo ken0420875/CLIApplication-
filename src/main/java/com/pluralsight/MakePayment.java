4cd2fa774a77fdb6282f5ebbabf6e3fe32003377
Muhammad Mustafa <acmuhammadmustafa@gmail.com>
2026-01-16 11:29:16 -0500

    Changes to makePayment

--- src/main/java/com/pluralsight/MakePayment.java
+++ src/main/java/com/pluralsight/MakePayment.java
@@ -3,25 +3,22 @@ package com.pluralsight;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class MakePayment {
 
     public static void makePayment(Scanner scanner) {
-        System.out.print(" Please fill in the Date (MM/DD/YYYY): ");
-        String Sysdate = scanner.nextLine();
-        /// Today's time^^^
-        // different time slow
-        //Want these to be inputted into csv
-        System.out.println("Current time (0:00)");
-        String Systimestamp = scanner.nextLine();
+        LocalDate today = LocalDate.now();
+        LocalTime time = LocalTime.now();
 
         String description = ConsoleHelper.promptForString("Please enter the description");
         String vendorName = ConsoleHelper.promptForString("Please enter vendor name");
         double amount = ConsoleHelper.promptForDouble("Please enter amount to be paid");
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("transactions.csv", true))) {
-            writer.write(Sysdate + "|" + Systimestamp + "|" + description + "|" + vendorName + "|" + -amount + "\n");
+            writer.write(today + "|" + time + "|" + description + "|" + vendorName + "|" + -amount + "\n");
             System.out.println("Payment completed");
         } catch (IOException ex) {
             System.err.println("Error completing payment: " + ex.getMessage());
